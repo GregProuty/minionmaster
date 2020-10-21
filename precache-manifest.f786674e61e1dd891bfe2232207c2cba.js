9dbca92cb88a24258c26ff4e40a888165fa190fa
grey <grey@pop-os.localdomain>
2020-10-20 17:04:29 -0700

    Updates

---
+++ precache-manifest.f786674e61e1dd891bfe2232207c2cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1085a76e49bd942d70bba154fcb738",
+    "url": "/minionmaster/index.html"
+  },
+  {
+    "revision": "241361c89588f2c407ee",
+    "url": "/minionmaster/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7fddc83397ab8379528",
+    "url": "/minionmaster/static/js/2.8804f1e3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/minionmaster/static/js/2.8804f1e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241361c89588f2c407ee",
+    "url": "/minionmaster/static/js/main.4ec12698.chunk.js"
+  },
+  {
+    "revision": "39cf55b6c3278ea8836d",
+    "url": "/minionmaster/static/js/runtime-main.c6571bd8.js"
+  }
+]);
